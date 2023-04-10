9c91b62f30e1e7aaff3ef9d528d3d521c3e64708
demilv <gonzalo.cano@bootcamp-upgrade.com>
2023-04-10 22:20:42 +0200

    S8 falta callbacks ejercicio solo

--- S8/3.async-await/exercise-2.js
+++ S8/3.async-await/exercise-2.js
@@ -1,17 +1,30 @@
 // Convierte la siguiente función con un fetch utilizando async-await. Recuerda que para usar .fetch() tendrás que probar el ejercicio en el navegador;
 
 // ````js
-const getCharacters = async () => {
-    let respuesta = await fetch('https://rickandmortyapi.com/api/character')
-    let res = await respuesta.json()
-    return res
-}
+// const getCharacters = async () => {
+//     let respuesta = await fetch('https://rickandmortyapi.com/api/character')
+//     let res = await respuesta.json()
+//     return res
+// }
 
-const init = async () => {
-    const characters = await getCharacters()
-    console.log(characters.results)
-}
+// const init = async () => {
+//     const characters = await getCharacters()
+//     console.log(characters.results)
+// }
 
-init()
+// init()
 
 // ````
+
+const getCharacters = async () =>{
+    fetch('https://rickandmortyapi.com/api/character')
+    .then(res => res.json())
+    .then(characters => console.log(characters));
+}
+
+const start = async() =>{
+    const chars = await getCharacters();   
+    console.log(chars) 
+}
+
+start()
