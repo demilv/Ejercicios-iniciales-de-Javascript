6776fa8755f2da678492c90f3349621a71168871
demilv <gonzalo.cano@bootcamp-upgrade.com>
2023-04-11 14:17:14 +0200

    Ahora esta todo, falta extra de S7 y de S8

--- S7/4.map/exercise-3.js
+++ S7/4.map/exercise-3.js
@@ -4,6 +4,16 @@
 const cities = [{isVisited:true, name: 'Tokyo'}, {isVisited:false, name: 'Madagascar'},{isVisited:true, name: 'Amsterdam'}, {isVisited:false, name: 'Seul'}];
 // ```
 
+let ciudades = cities.map((ciudad) => ({
+    nombre : ciudad.name
+   }));
 
-
+for (lugares in ciudades){
+    for(lugares2 in cities){
+        if(ciudades[lugares].nombre == cities[lugares2].name && cities[lugares2].isVisited == true){
+            ciudades[lugares].nombre += "(Visitado)"
+        }
+    }
+}  
+console.log(ciudades)
 // :(
